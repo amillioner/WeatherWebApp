2488c8b1846bbc8c82271cd64ccac8173d8a1cf0
Anwar Akhror <anwar.akhror@gmail.com>
2024-09-05 18:14:59 -0400

    ssl false#

--- Weather.Api.Web/Program.cs
+++ Weather.Api.Web/Program.cs
@@ -16,6 +16,7 @@ builder.Services.RegisterGeoServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen();
 
 builder.Services
     .AddEndpointsApiExplorer()
